e66b48ccbad0fa25a62e52c4066ed74f8d5a4f3c
Давлетбакова Нелли Александровна <davletbakova.na1258@oksei.onmicrosoft.com>
2021-09-29 14:05:54 +0500

    test edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину отрезка A ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите длину отрезка B ");
+            int b = int.Parse(Console.ReadLine());
+            int c = 0;
+            while ((a - b) >= 0)
+            {
+                a -= b;
+                c += 1;
+            }
+            Console.WriteLine($"Колличество отрезков B ={c} ");
+            Console.ReadKey();
+        }
+    }
+}
