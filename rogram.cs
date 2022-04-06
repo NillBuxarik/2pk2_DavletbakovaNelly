053ee7c47ffdbb5c4fd1449c62ecb499bf2411fe
NillBuxarik <89990318+NillBuxarik@users.noreply.github.com>
2022-04-06 11:09:24 +0500

    Текстовый редактор

---
+++ rogram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Sum(number));
+
+        }
+        static int Sum(int number)
+        {
+            int result = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
+
+
