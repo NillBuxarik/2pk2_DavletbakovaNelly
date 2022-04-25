fd3e802eb2cf8af365c37b64085640f81d49f360
NillBuxarik <89990318+NillBuxarik@users.noreply.github.com>
2022-04-26 00:00:34 +0500

    Test

---
+++ PZ_2_10_events/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PZ_2_10_events
+{
+    class Programm
+    {
+        static void Main()
+        {// генерация значений и запись в массив Array.Add
+            ArrayList Array = new ArrayList();
+
+            for (int i = 0, k = 8; i < 8; i++)
+            {
+                Array.Add(new Train("name" + i.ToString(), k, DateTime.Now));
+                k--;
+            }
+            //вывод сгенерированных значений 
+            foreach (Train c in Array)
+            {
+                Console.WriteLine(c.ToString());
+            }
+            string str;
+            bool b = true;
+            int count = 0;
+            while (b)
+            {
+                Console.WriteLine("\n\nВведите номер поезда для поиска");
+                str = Console.ReadLine();
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (char.IsDigit(str[i]) && (Convert.ToInt32(str[i]) <= 8 || Convert.ToInt32(str[i]) >= 0)) count++;
+                }
+
+                if (count == str.Length)
+                {
+                    foreach (Train c in Array)
+                    {
+                        if (str == c.number.ToString()) Console.WriteLine(c.ToString());
+                    }
+                    b = false;
+                }
+                else Console.WriteLine("Повторите попытку");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PZ_2_10_events/Train.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PZ_2_10_events
+{
+    public struct Train
+    {
+        string name;
+        public int number;
+        DateTime date;
+
+        public Train(string name, int number, DateTime date)
+        {
+            this.name = name;
+            this.number = number;
+            this.date = date;
+        }
+
+        public string CompareTo(object obj)
+        {
+            Train a = (Train)obj;
+
+            if (this.name.CompareTo(a.name) == -1)
+                return a.name;
+            if (this.name.CompareTo(a.name) == 0)
+                return a.name;
+            else return a.name;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Название пункта назначения " + this.name + " \nНомер поезда " + number + " \nВремя отправления" + date);
+        }
+    }
+
+}
